6f6f58c85b191e4915013696da54bdffa3a5d843
catom <tom.calvo@ynov.com>
2024-03-16 12:26:08 +0100

    add shearch collision function

---
+++ src/shearchCollision.js
@@ -0,0 +1,10 @@
+function searchCollision(map, playerPositionX, playerPositionY) {
+    //check different type of collision
+    if (map[[playerPositionX][playerPositionY]] === 0 || map[[playerPositionX][playerPositionY]] === 4){
+        return 0;//no collision
+    } else if (map[[playerPositionX][playerPositionY]] === 1){
+        return 1;//wall collision
+    } else if (map[[playerPositionX][playerPositionY]] === 2){
+        return 2;//box collision
+    }
+}
